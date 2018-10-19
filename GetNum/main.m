602dc9724aed0e0b8c9db49f87594f5785a69f37
邱嘉敏 <jiaminqi_u@126.com>
2018-10-19 11:37:53 +0800

    Initial Commit

---
+++ GetNum/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  GetNum
+//
+//  Created by Hanrun on 2018/10/19.
+//  Copyright © 2018年 Carvin. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
